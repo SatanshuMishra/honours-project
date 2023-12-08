e67eb58d44aa91e046e8d93304aa7afda3ad0e69
Satanshu Mishra <satanshumishra@outlook.com>
2023-12-08 13:08:09 -0800

    Added Loading State Condition

--- app/user-auth/page.tsx
+++ app/user-auth/page.tsx
@@ -1,21 +1,35 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LoginSVG from "../../public/unDraw_Login.svg";
 import RegisterSVG from "../../public/unDraw_Register.svg";
 import LogIn from "./login";
 import Register from "./register";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 function UserAuth() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true);
+    const token = Cookies.get("token");
+    if (token) {
+      router.replace("/dashboard");
+      return;
+    }
+    setIsLoading(false);
+  }, [router]);
   const [isSignIn, setIsSignIn] = useState(true);
   return (
     <>
-      {isSignIn ? (
-        <LogIn setSignIn={setIsSignIn} displaySignIn={isSignIn} />
-      ) : (
-        <Register setSignIn={setIsSignIn} displaySignIn={isSignIn} />
-      )}
+      {!isLoading &&
+        (isSignIn ? (
+          <LogIn setSignIn={setIsSignIn} displaySignIn={isSignIn} />
+        ) : (
+          <Register setSignIn={setIsSignIn} displaySignIn={isSignIn} />
+        ))}
     </>
   );
 }
