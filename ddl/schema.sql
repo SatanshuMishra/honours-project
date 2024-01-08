c993e1fe757ef9b8a5323db34435d1fe9dbd6ba5
Satanshu Mishra <satanshumishra@outlook.com>
2024-01-07 17:06:27 -0800

    Updates to SQL
    - Updated SQL to include Questions and Answers Tables. This will be where the JSON data is loaded into.

--- ddl/schema.sql
+++ ddl/schema.sql
@@ -14,3 +14,20 @@ CREATE TABLE student (
   password VARCHAR(255) NOT NULL,
   completedBonusContent BOOLEAN NOT NULL DEFAULT FALSE
 ) ENGINE = InnoDB;
+
+CREATE TABLE question (
+	questionID BINARY(36) NOT NULL PRIMARY KEY,
+	difficulty INTEGER NOT NULL,
+	CONSTRAINT Diff_Consrt CHECK (difficulty IN (1, 2, 3)),
+	question TEXT NOT NULL,
+	code TEXT
+) ENGINE = InnoDB;
+
+CREATE TABLE answer (
+	answerID BINARY(36) NOT NULL PRIMARY KEY,
+	questionID BINARY(36) NOT NULL,
+	answerDescription TEXT NOT NULL,
+	answerExplanation TEXT NOT NULL,
+	isCorrect BOOLEAN NOT NULL,
+	FOREIGN KEY (questionID) REFERENCES question(questionID)
+) ENGINE = InnoDB;
