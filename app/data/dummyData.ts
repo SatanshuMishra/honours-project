56098e36cbc19cd3a471448e713d78695dfa883c
Satanshu Mishra <satanshumishra@outlook.com>
2024-04-08 15:58:54 -0700

    Added Empty dummyData

---
+++ app/data/dummyData.ts
@@ -0,0 +1,11 @@
+export const dummyData: {
+  difficulty: number;
+  question: string;
+  code?: string;
+  answers: string[];
+  correct: number;
+  explanations: string[];
+  bloomTaxonomy: string;
+  timeTakenSeconds: number;
+  topic: string;
+}[] = [];
