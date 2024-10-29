73d6229c0a187c5a701eb98dcb301c10c2b8237f
Satanshu Mishra <satanshumishra@outlook.com>
2024-10-28 20:51:36 -0600

    Updates & Optimizations
    - Optimized questionnaire implementation for better clarity and maintainability.
    - Updated styles on login page.

--- app/user-auth/login.tsx
+++ app/user-auth/login.tsx
@@ -60,7 +60,7 @@ export default function LogIn({ setSignIn, displaySignIn }: AuthProps) {
 							<form className="w-full" onSubmit={formik.handleSubmit}>
 								<Input name="Username" htmlFor="username" handleOnBlur={formik.handleBlur} onChangeFunction={formik.handleChange} description="This username should have been pre-assigned to you by your instructor." />
 								<Input name="Password" htmlFor="password" isPassword={true} handleOnBlur={formik.handleBlur} onChangeFunction={formik.handleChange} />
-								<button className="w-full px-4 py-2 my-4 font-semibold text-xl text-white bg-blue-600 hover:bg-blue-700 rounded-lg" type="submit">Start Learning</button>
+								<button className="w-full px-4 py-2 my-4 font-semibold text-xl text-white bg-blue-600 hover:bg-blue-700 rounded-lg" type="submit" style={{background: "#0185FF"}}>Start Learning</button>
 							</form>
 							<div className="w-full">
 								<p className="w-auto text-lg font-light py-2 text-black">
@@ -87,7 +87,7 @@ export default function LogIn({ setSignIn, displaySignIn }: AuthProps) {
 					</div>
 				</section>
 				<section className="hidden mobile:inline-block w-full h-full p-4">
-					<div className="w-full h-full bg-blue-600 rounded-lg flex flex-col justify-center items-center">
+					<div className="w-full h-full bg-[#3182ce] rounded-lg flex flex-col justify-center items-center" style={{background: "#0185FF"}}>
 						<Image src={LoginSVG} alt="LoginSVG" className="h-fit w-full p-8" priority={true} />
 					</div>
 				</section>
