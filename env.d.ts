45548b6804ef2741b4e34d5fa9a5f60ca5324cfd
Satanshu Mishra <satanshumishra@outlook.com>
2024-11-07 15:49:23 -0700

    Updated .env & Student Code Insertion
    - Updated `.env` to add `BASE_URL`.
    - Added codes files.
    - Added route to insert codes.
    - Added button to insert codes on the AdminToolbar.

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@ declare namespace NodeJS {
 		readonly DATABASE_URL: string
 		readonly NEXT_PUBLIC_NUMBER_OF_QUESTIONS: string
 		readonly NEXT_PUBLIC_BONUS_REQUIREMENT: string
+		readonly NEXT_PUBLIC_BASE_URL: string
 		readonly JWT_SECRET: string
 		readonly PASSWORD_ENCRYPTION_KEY: string
 	}
